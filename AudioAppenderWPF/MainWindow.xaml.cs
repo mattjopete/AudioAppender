f0ede607e2cfa5ee48b5b049a9e1df0321bb5d4f
Matt Peterein <matthewpeterein@gmail.com>
2019-02-03 11:39:53 -0600

    Update textbox with value from filepicker

--- AudioAppenderWPF/MainWindow.xaml.cs
+++ AudioAppenderWPF/MainWindow.xaml.cs
@@ -18,16 +18,21 @@ namespace AudioAppenderWPF
 
         private void OpenFile_Click(object sender, RoutedEventArgs e)
         {
-            string file;
+            if(string.IsNullOrWhiteSpace(OutputFileTextbox.Text))
+            {
+                //TODO: display message
+                return;
+            }
+
+            string file = string.Empty;
             var dialog = new OpenFileDialog();
             dialog.Filter = "(*.mp3)|*.mp3";
             if(dialog.ShowDialog() == true)
             {
                 file = dialog.FileName;
             }
-            //TODO: Get textbox control from stackpanel  and save filename to it.
-            var parent = this.Parent;
-            
+            var textbox = GetButtonTextbox((Button)sender);
+            textbox.Text = file;
         }
 
         private void SaveFile_Click(object sender, RoutedEventArgs e)
@@ -45,7 +50,7 @@ namespace AudioAppenderWPF
                         if( property.GetType() == typeof(TextBox))
                         {
                             var textbox = (TextBox)property;
-                            if(string.IsNullOrWhiteSpace(textbox.Text))
+                            if(!string.IsNullOrWhiteSpace(textbox.Text))
                             {
                                 files.Add(textbox.Text);
                             }
@@ -55,7 +60,7 @@ namespace AudioAppenderWPF
             }
 
             var output = audioConcatenator.Concatenate(files.ToArray());
-            using (var fileStream = File.Create(@"C:\Users\truck\Music\condatenated.mp3"))
+            using (var fileStream = File.Create(OutputFileTextbox.Text))
             {
                 output.Seek(0, SeekOrigin.Begin);
                 output.CopyTo(fileStream);
@@ -66,12 +71,31 @@ namespace AudioAppenderWPF
 
         private void OuputFilePicker_Click(object sender, RoutedEventArgs e)
         {
-
+            string file= string.Empty;
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "(*.mp3)|*.mp3";
+            if (dialog.ShowDialog() == true)
+            {
+                file = dialog.FileName;
+            }
+            OutputFileTextbox.Text = file;
         }
 
         private void AddFileSource_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private TextBox GetButtonTextbox(Button button)
+        {
+            var stackPanel = (StackPanel)button.Parent;
+            foreach(var child in stackPanel.Children)
+            {
+                if(child.GetType() == typeof(TextBox))
+                    return (TextBox)child;
+            }
+
+            return null;//Have to do this becuase UIElementCollection doesn't have Linq support
         }
     }
 }
