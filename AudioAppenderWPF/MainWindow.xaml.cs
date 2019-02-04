82a4d513be3eced5a1c9a86bc42d6f1eadaaba84
Matt Peterein <matthewpeterein@gmail.com>
2019-02-04 13:53:15 -0600

    Make app functional

--- AudioAppenderWPF/MainWindow.xaml.cs
+++ AudioAppenderWPF/MainWindow.xaml.cs
@@ -2,14 +2,16 @@
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using Microsoft.Win32;
+using SourceChord.FluentWPF;
 
 namespace AudioAppenderWPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow 
+    public partial class MainWindow
     {
         public MainWindow()
         {
@@ -18,16 +20,12 @@ namespace AudioAppenderWPF
 
         private void OpenFile_Click(object sender, RoutedEventArgs e)
         {
-            if(string.IsNullOrWhiteSpace(OutputFileTextbox.Text))
-            {
-                //TODO: display message
-                return;
-            }
+           
 
             string file = string.Empty;
             var dialog = new OpenFileDialog();
             dialog.Filter = "(*.mp3)|*.mp3";
-            if(dialog.ShowDialog() == true)
+            if (dialog.ShowDialog() == true)
             {
                 file = dialog.FileName;
             }
@@ -37,62 +35,86 @@ namespace AudioAppenderWPF
 
         private void SaveFile_Click(object sender, RoutedEventArgs e)
         {
-            var audioConcatenator = new AudioConcatenator();
 
-            var files = new List<string>();
-            foreach(var selectedFile in FileStack.Children)
+            var saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
             {
-                if(selectedFile is StackPanel)
+                var audioConcatenator = new AudioConcatenator();
+
+                var files = new List<string>();
+                foreach (var selectedFile in FileStack.Children)
                 {
-                    var selectedFileStack = (StackPanel) selectedFile;
-                    foreach(var property in selectedFileStack.Children)
+                    if (selectedFile is StackPanel)
                     {
-                        if( property.GetType() == typeof(TextBox))
+                        var selectedFileStack = (StackPanel)selectedFile;
+                        foreach (var property in selectedFileStack.Children)
                         {
-                            var textbox = (TextBox)property;
-                            if(!string.IsNullOrWhiteSpace(textbox.Text))
+                            if (property.GetType() == typeof(TextBox))
                             {
-                                files.Add(textbox.Text);
+                                var textbox = (TextBox)property;
+                                if (!string.IsNullOrWhiteSpace(textbox.Text))
+                                {
+                                    files.Add(textbox.Text);
+                                }
                             }
                         }
                     }
                 }
+
+                var output = audioConcatenator.Concatenate(files.ToArray());
+                using (var fileStream = File.Create(saveFileDialog.FileName))
+                {
+                    output.Seek(0, SeekOrigin.Begin);
+                    output.CopyTo(fileStream);
+                    output.Flush();
+                }
             }
 
-            var output = audioConcatenator.Concatenate(files.ToArray());
-            using (var fileStream = File.Create(OutputFileTextbox.Text))
-            {
-                output.Seek(0, SeekOrigin.Begin);
-                output.CopyTo(fileStream);
-                output.Flush();
-            }
-
-        }
-
-        private void OuputFilePicker_Click(object sender, RoutedEventArgs e)
-        {
-            string file= string.Empty;
-            var dialog = new OpenFileDialog();
-            dialog.Filter = "(*.mp3)|*.mp3";
-            if (dialog.ShowDialog() == true)
-            {
-                file = dialog.FileName;
-            }
-            OutputFileTextbox.Text = file;
         }
 
         private void AddFileSource_Click(object sender, RoutedEventArgs e)
         {
+            var newFileSelector = new StackPanel();
+            PointerTracker.SetEnabled(newFileSelector, true);
+            newFileSelector.Margin = new Thickness(3);
+            newFileSelector.HorizontalAlignment = HorizontalAlignment.Left;
+            newFileSelector.Orientation = Orientation.Horizontal;
+            newFileSelector.Width = 350;
 
+            var textBoxRevealStyle = (Style)Application.Current.Resources["TextBoxRevealStyle"];
+            var newTextbox = new TextBox
+            {
+                Width = 275,
+                Style = textBoxRevealStyle
+            };
+            newFileSelector.Children.Add(newTextbox);
+
+            var buttonRevealStyle = (Style)Application.Current.Resources["ButtonRevealStyle"];
+            var systemBaseHighColorBrush = (Brush)Application.Current.Resources["SystemBaseHighColorBrush"];
+            var newButton = new Button
+            {
+                Style = buttonRevealStyle,
+                Foreground = systemBaseHighColorBrush,
+                Background = AccentColors.ImmersiveSystemAccentDark1Brush,
+                Content = "Open",
+                Width = 75
+                
+            };
+            newButton.Click += OpenFile_Click;
+            newFileSelector.Children.Add(newButton);
+
+            FileStack.Children.Insert(FileStack.Children.Count -1, newFileSelector);
         }
 
         private TextBox GetButtonTextbox(Button button)
         {
             var stackPanel = (StackPanel)button.Parent;
-            foreach(var child in stackPanel.Children)
+            foreach (var child in stackPanel.Children)
             {
-                if(child.GetType() == typeof(TextBox))
+                if (child.GetType() == typeof(TextBox))
+                {
                     return (TextBox)child;
+                }
             }
 
             return null;//Have to do this becuase UIElementCollection doesn't have Linq support
