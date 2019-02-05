e4c731c93cfd9c0e72fa1db7277d8f02c65a142a
Matt Peterein <matthewpeterein@gmail.com>
2019-02-05 09:21:03 -0600

    Set default extension

--- AudioAppenderWPF/MainWindow.xaml.cs
+++ AudioAppenderWPF/MainWindow.xaml.cs
@@ -38,6 +38,7 @@ namespace AudioAppenderWPF
 
             var saveFileDialog = new SaveFileDialog();
             saveFileDialog.Filter = "(*.mp3)|*.mp3";
+            saveFileDialog.DefaultExt = "mp3";
             if (saveFileDialog.ShowDialog() == true)
             {
                 var audioConcatenator = new AudioConcatenator();
